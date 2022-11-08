3c8d956320f33fc54e8370ef041179fa223d7f13
jennzheng12315 <jennzheng12315@gmail.com>
2022-11-07 19:16:30 -0500

    added all pages

--- app.py
+++ app.py
@@ -3,6 +3,22 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-    # return render_template('index.html', page_title='Home')
+@app.route("/index")
+@app.route("/index.html")
+def root():
+    return render_template('index.html', page_title='Home')
+
+@app.route("/about")
+@app.route("/about.html")
+def about():
+    return render_template('about.html', page_title='About')
+
+@app.route("/explore")
+@app.route("/explore.html")
+def explore():
+    return render_template('explore.html', page_title='Explore')
+
+@app.route("/tell-your-story")
+@app.route("/tell-your-story.html")
+def tell_your_story():
+    return render_template('tell-your-story.html', page_title='Tell Your Story')
